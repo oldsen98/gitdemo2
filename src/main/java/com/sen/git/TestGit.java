9470f32a6379318fe2614bee2be2541e889dee37
oldsen <2365665276@qq.com>
2023-04-22 15:04:04 +0800

    second commit

--- src/main/java/com/sen/git/TestGit.java
+++ src/main/java/com/sen/git/TestGit.java
@@ -12,6 +12,7 @@ package com.sen.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!!");
+        System.out.println("Hello Git2!!");
     }
 }
 
