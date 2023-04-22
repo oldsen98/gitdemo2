42210057c28c9ca25548cf86afe037d45e93deb9
oldsen <2365665276@qq.com>
2023-04-22 13:14:40 +0800

    first commit

---
+++ src/main/java/com/sen/git/TestGit.java
@@ -0,0 +1,17 @@
+package com.sen.git;
+
+/**
+ * ClassName: TestGit
+ * Package: com.sen.git
+ * Description:
+ *
+ * @Author sen
+ * @Create 2023-04-22 12:49
+ * @Version 1.0
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!");
+    }
+}
+
